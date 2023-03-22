94438355ae4e1585593fe0d36662f403e58b3f1a
kwameasantenkrumah <kwameasantenkrumah@gmail.com>
2023-03-22 14:34:30 -0700

    A function that prints the sign of a number.

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+* print_sign - function that prints the sign of a number
+*
+* @n: character to be checked
+*
+* Return: 1, 0 and -1
+*/
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		_putchar(',');
+		_putchar(' ');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		_putchar(',');
+		_putchar(' ');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		_putchar(',');
+		_putchar(' ');
+		return (-1);
+	}
+}
